9ad9d6c60a79ebd4312fab999e9a18266b1611bd
esivanov <esivanov@mts.ru>
2023-04-10 11:58:56 +0300

    Added keys for pageheader

--- lua/pageheader.lua
+++ lua/pageheader.lua
@@ -1,2 +1,9 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+
+vim.keymap.set('n', '<leader>p', [[:BufferLinePick<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>tp', [[:BufferLineTogglePin<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>cl', [[:BufferLineCloseLeft<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>cr', [[:BufferLineCloseRight<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>bp', [[:BufferLineCyclePrev<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>bn', [[:BufferLineCycleNext<CR>]], { silent = true })
